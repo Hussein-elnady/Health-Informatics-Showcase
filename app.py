702ffa716d05a86f9134d7f14df4a342722ae4c9
Hussein Elnady <hussein_elnady@yahoo.com>
2024-02-15 23:36:12 +0200

    initial commit

--- app.py
+++ app.py
@@ -26,10 +26,13 @@ img_lottie_animation = Image.open("images/yt_lottie_animation.jpg")
 
 # ---- HEADER SECTION ----
 with st.container():
+    st.image("images/me.jpg", width=200)
+    st.write("")
     st.subheader("Hi, I am Hussein Elnady :wave:")
-    st.title ("A Health Informatics Specialist")
-    st.write("I am passionate about finding ways to use Python and VBA to be more efficient and effective in business settings")
-    st.write("[learn more >](https://www.linkedin.com/in/hussein-elnady-94ha)")
+    st.title("A Health Informatics Specialist")
+    st.write("I am passionate about finding ways to use Python and VBA to be more efficient and effective in business settings.")
+    st.write("[Learn more](https://www.linkedin.com/in/hussein-elnady-94ha)")
+
 
 # ---- What I Do ---- 
 with st.container():
@@ -73,7 +76,7 @@ with st.container():
         )
         st.markdown("[Watch Video...](https://youtube/)")
 
-# ----  Form ---- 
+
 with st.container():
     st.write("---")
     image_column, text_column = st.columns((1, 2))
@@ -90,76 +93,82 @@ with st.container():
         st.markdown("[Watch Video...](https://youtube/)")
 
 # Experience
-st.header("Experience")
+with st.container():
+    st.write("---")
+    st.header("Experience")
+    st.subheader("Health Informatician")
+    st.write("Benha - Qalyubia - Egypt")
+    st.write("General Organization for Teaching Hospitals and Institutes (GOTHI)")
+    st.write("Feb 2022 - Present")
+    st.write("- Developing and implementing electronic health record (EHR) systems.")
+    st.write("- Training and supporting clinical staff on EHR systems.")
 
-st.subheader("Health Informatician")
-st.write("Benha - Qalyubia - Egypt")
-st.write("General Organization for Teaching Hospitals and Institutes (GOTHI)")
-st.write("Feb 2022 - Present")
-st.write("- Developing and implementing electronic health record (EHR) systems.")
-st.write("- Training and supporting clinical staff on EHR systems.")
+with st.container():
+    # Education
+    st.write("---")
+    st.header("Education")
 
-# Education
-st.header("Education")
+    st.subheader("Bachelor of Nursing")
+    st.write("Benha University")
+    st.write("2012-2016")
 
-st.subheader("Bachelor of Nursing")
-st.write("Benha University")
-st.write("2012-2016")
+    st.subheader("Health Informatics & Health Information Management")
+    st.write("Egyptian Fellowship")
+    st.write("2021-2024")
 
-st.subheader("Health Informatics & Health Information Management")
-st.write("Egyptian Fellowship")
-st.write("2021-2024")
+with st.container():
+    # Skills
+    st.header("Skills")
 
-# Skills
-st.header("Skills")
+    st.subheader("Technical Skills")
+    st.write("- Experience with EHR systems and healthcare data analysis software.")
+    st.write("- Knowledgeable in HL7 FHIR and other healthcare data standards and protocols.")
+    st.write("- Experience with programming languages like SQL, Python, and R.")
+    st.write("- Experience with data visualization tools like Power BI.")
 
-st.subheader("Technical Skills")
-st.write("- Experience with EHR systems and healthcare data analysis software.")
-st.write("- Knowledgeable in HL7 FHIR and other healthcare data standards and protocols.")
-st.write("- Experience with programming languages like SQL, Python, and R.")
-st.write("- Experience with data visualization tools like Power BI.")
+    st.subheader("Clinical Skills")
+    st.write("- Knowledge of medical terminology and procedures.")
+    st.write("- Understanding of healthcare privacy and security regulations.")
 
-st.subheader("Clinical Skills")
-st.write("- Knowledge of medical terminology and procedures.")
-st.write("- Understanding of healthcare privacy and security regulations.")
+    st.subheader("Communication and Interpersonal Skills")
+    st.write("- Ability to work effectively with various stakeholders.")
+    st.write("- Ability to provide excellent customer service.")
 
-st.subheader("Communication and Interpersonal Skills")
-st.write("- Ability to work effectively with various stakeholders.")
-st.write("- Ability to provide excellent customer service.")
+    st.subheader("Problem-solving and Analytical Skills")
+    st.write("- Ability to identify and solve problems.")
 
-st.subheader("Problem-solving and Analytical Skills")
-st.write("- Ability to identify and solve problems.")
+    st.subheader("Organizational Skills")
+    st.write("- Ability to manage multiple tasks and projects simultaneously.")
 
-st.subheader("Organizational Skills")
-st.write("- Ability to manage multiple tasks and projects simultaneously.")
+with st.container():
+    # Experience Details
+    st.header("Experience Details")
 
-# Experience Details
-st.header("Experience Details")
+    st.subheader("Intensive Care Clinical Nurse Specialist")
+    st.write("Mar 2020 - Oct 2021")
+    st.write("Qaha Central Hospital")
+    st.write("- Provide direct patient care to critically ill patients.")
+    st.write("- Assess, plan, implement, and evaluate patient care plans.")
 
-st.subheader("Intensive Care Clinical Nurse Specialist")
-st.write("Mar 2020 - Oct 2021")
-st.write("Qaha Central Hospital")
-st.write("- Provide direct patient care to critically ill patients.")
-st.write("- Assess, plan, implement, and evaluate patient care plans.")
+    st.subheader("Registered Nurse")
+    st.write("Sep 2016 - Oct 2018")
+    st.write("Nasser Institute Hospital")
+    st.write("- Assess patients' needs and develop care plans.")
+    st.write("- Collaborate with healthcare professionals to ensure comprehensive care.")
 
-st.subheader("Registered Nurse")
-st.write("Sep 2016 - Oct 2018")
-st.write("Nasser Institute Hospital")
-st.write("- Assess patients' needs and develop care plans.")
-st.write("- Collaborate with healthcare professionals to ensure comprehensive care.")
+with st.container():
+    # Licenses & Certifications
+    st.header("Licenses & Certifications")
 
-# Licenses & Certifications
-st.header("Licenses & Certifications")
-
-st.write("- Introduction to Cybersecurity - Cisco")
-st.write("- Data Literacy Essentials - SAS")
-st.write("- The Non-Technical Skills of Effective Data Scientists - LinkedIn")
-st.write("- Introduction to IoT - Cisco")
-st.write("- Linear Algebra for Machine Learning and Data Science - DeepLearning.AI")
-st.write("- Database Fundamentals - Information Technology Institute (ITI)")
-st.write("- Calculus for Machine Learning and Data Science - DeepLearning.AI")
-st.write("- Probability & Statistics for Machine Learning & Data Science - DeepLearning.AI")
-st.write("- Mathematics for Machine Learning and Data Science - DeepLearning.AI")
+    st.write("- Introduction to Cybersecurity - Cisco")
+    st.write("- Data Literacy Essentials - SAS")
+    st.write("- The Non-Technical Skills of Effective Data Scientists - LinkedIn")
+    st.write("- Introduction to IoT - Cisco")
+    st.write("- Linear Algebra for Machine Learning and Data Science - DeepLearning.AI")
+    st.write("- Database Fundamentals - Information Technology Institute (ITI)")
+    st.write("- Calculus for Machine Learning and Data Science - DeepLearning.AI")
+    st.write("- Probability & Statistics for Machine Learning & Data Science - DeepLearning.AI")
+    st.write("- Mathematics for Machine Learning and Data Science - DeepLearning.AI")
 
 
 # ---- CONTACT ----
